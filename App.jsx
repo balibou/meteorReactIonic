245f4683e5dfc97a63e560fc05ca577ee34c354b
balibou <benjamin.cherion@gmail.com>
2015-12-02 22:14:57 +0100

    possibility to create and read tasks with mongo

--- App.jsx
+++ App.jsx
@@ -1,23 +1,45 @@
 App = React.createClass({
-  getTasks() {
-    return [
-      { _id: 1, text: "This is task 1" },
-      { _id: 2, text: "This is task 2" },
-      { _id: 3, text: "This is task 3" }
-    ];
+  mixins: [ReactMeteorData],
+
+  getMeteorData() {
+    return {
+      tasks: Tasks.find({}, {sort: {createdAt: -1}}).fetch()
+    }
   },
 
   renderTasks() {
-    return this.getTasks().map((task) => {
+    return this.data.tasks.map((task) => {
       return <Task key={task._id} task={task} />;
     });
   },
+
+  handleSubmit(event) {
+      event.preventDefault();
+
+      // Find the text field via the React ref
+      var text = React.findDOMNode(this.refs.textInput).value.trim();
+
+      Tasks.insert({
+        text: text,
+        createdAt: new Date() // current time
+      });
+
+      // Clear form
+      React.findDOMNode(this.refs.textInput).value = "";
+    },
 
   render() {
     return (
       <div className="container">
         <header>
           <h1>Todo List</h1>
+
+          <form className="new-task" onSubmit={this.handleSubmit} >
+            <input
+              type="text"
+              ref="textInput"
+              placeholder="Type to add new tasks" />
+          </form>
         </header>
 
         <ul>
--- meteoreact.jsx
+++ meteoreact.jsx
@@ -1,3 +1,5 @@
+Tasks = new Mongo.Collection("tasks");
+
 if (Meteor.isClient) {
   Meteor.startup(function () {
     React.render(<App />, document.getElementById("render-target"));
