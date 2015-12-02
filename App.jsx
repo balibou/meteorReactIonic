fe3be79492be9721e6b905475376d5bd6e396880
balibou <benjamin.cherion@gmail.com>
2015-12-02 23:10:45 +0100

    temporary UI client side

--- App.jsx
+++ App.jsx
@@ -1,9 +1,22 @@
 App = React.createClass({
   mixins: [ReactMeteorData],
 
-  getMeteorData() {
+  getInitialState() {
     return {
-      tasks: Tasks.find({}, {sort: {createdAt: -1}}).fetch()
+      hideCompleted: false
+    }
+  },
+
+  getMeteorData() {
+    let query = {};
+
+    if (this.state.hideCompleted) {
+      // If hide completed is checked, filter tasks
+      query = {checked: {$ne: true}};
+    }
+    return {
+      tasks: Tasks.find(query, {sort: {createdAt: -1}}).fetch(),
+      incompleteCount: Tasks.find({checked: {$ne: true}}).count()
     }
   },
 
@@ -26,11 +39,26 @@ App = React.createClass({
       React.findDOMNode(this.refs.textInput).value = "";
     },
 
+  toggleHideCompleted() {
+    this.setState({
+      hideCompleted: ! this.state.hideCompleted
+    });
+  },
+
   render() {
     return (
       <div className="container">
         <header>
-          <h1>Todo List</h1>
+          <h1>Todo List ({this.data.incompleteCount})</h1>
+
+          <label className="hide-completed">
+            <input
+              type="checkbox"
+              readOnly={true}
+              checked={this.state.hideCompleted}
+              onClick={this.toggleHideCompleted} />
+            Hide Completed Tasks
+          </label>
 
           <form className="new-task" onSubmit={this.handleSubmit} >
             <input
