9ee5555ef06c26c8f1819244c7fd21ac8300d20c
balibou <benjamin.cherion@gmail.com>
2016-01-09 12:55:57 +0100

    mobile config file added

---
+++ mobile-config.js
@@ -0,0 +1,7 @@
+App.info({
+  name: 'Mobile meteor starter',
+  description: 'Meteor starter for mobile app',
+  author: 'Benjamin Cherion',
+  email: 'benjamin.cherion@gmail.com',
+  website: 'https://github.com/balibou'
+});
